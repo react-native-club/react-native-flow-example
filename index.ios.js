f86e76320ab2b7d7dafdac31291abeb8bcc99426
Anita <a.nita@hotmail.co.uk>
2017-01-22 13:48:35 +0000

    initial setup

---
+++ index.ios.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+function multiply(n1: number, n2: number): number {
+  return n1 * n2;
+}
+
+export default class FlowExample extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{multiply(14, 14)}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('FlowExample', () => FlowExample);
